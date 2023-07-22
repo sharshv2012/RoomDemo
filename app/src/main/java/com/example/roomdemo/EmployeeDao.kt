962423e89b4181554c1ddcf5b5b5bcbe365eca1c
harsh <sharshvardhan759@gmail.com>
2023-07-22 11:11:09 +0530

    1st Commit : worked with dao interface and database class creation.

---
+++ app/src/main/java/com/example/roomdemo/EmployeeDao.kt
@@ -0,0 +1,31 @@
+package com.example.roomdemo
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+
+interface EmployeeDao {
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+    
+    @Query("SELECT * FROM `employee-table`")
+    fun fetchAllEmployees():Flow<List<EmployeeEntity>>
+
+    @Query("SELECT * FROM `employee-table` where id=:id")
+    fun fetchEmployeesById(id : Int):Flow<EmployeeEntity>
+
+
+
+}
